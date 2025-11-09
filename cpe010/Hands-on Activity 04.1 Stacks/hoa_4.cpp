f54148d6148e2965e659741d4370bb3c15a4cb3b
Francis Nikko Andoy <qfniandoy@tip.edu.ph>
2025-11-09 13:58:52 +0800

    added the code
    - add the code
    - commit
    - push

---
+++ cpe010/Hands-on Activity 04.1 Stacks/hoa_4.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+class Node {
+public:
+    int data;
+    Node *next;
+};
+
+Node *head = NULL, *tail = NULL;
+
+void push(int newData) {
+    Node *newNode = new Node;
+    newNode->data = newData;
+    newNode->next = head;
+    if (head == NULL) {
+        head = tail = newNode;
+    } else {
+        newNode->next = head;
+        head = newNode;
+    }
+}
+
+int pop() {
+    if (head == NULL) {
+        std::cout << "Stack Underflow." << std::endl;
+        return -1;
+    }
+    Node *temp = head;
+    int tempVal = temp->data;
+    head = head->next;
+    delete temp;
+    if (head == NULL) tail = NULL;
+    return tempVal;
+}
+
+void Top() {
+    if (head == NULL) {
+        std::cout << "Stack is Empty." << std::endl;
+        return;
+    } else {
+        std::cout << "Top of Stack: " << head->data << std::endl;
+    }
+}
+
+int main() {
+    push(1);
+    std::cout << "After the first PUSH top of stack is :";
+    Top();
+    push(5);
+    std::cout << "After the second PUSH top of stack is :";
+    Top();
+    pop();
+    std::cout << "After the first POP operation, top of stack is:";
+    Top();
+    pop();
+    std::cout << "After the second POP operation, top of stack :";
+    Top();
+    pop();
+    return 0;
+}
